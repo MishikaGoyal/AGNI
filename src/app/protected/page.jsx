cb95b2bde07f81fd5bc75a7ffe76caa80b6133b1
ashab-k <ashabkhan2000@gmail.com>
2024-08-29 15:00:47 +0530

    fixed issues with auth code

--- src/app/protected/page.jsx
+++ src/app/protected/page.jsx
@@ -1,4 +1,3 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
 export default function Page() {
   return <div>this is a protected page</div>;
 }
--- src/middleware.js
+++ src/middleware.js
@@ -4,20 +4,23 @@ import { NextResponse } from "next/server";
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export function middleware(req) {
-  /*  const token = req.cookies.get("token")?.value;
+  const token = req.cookies.get("token")?.value;
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
   try {
-    jwt.verify(token, JWT_SECRET);
-    return NextResponse.next();
-  } catch (error) {
-    console.error("JWT verification failed:", error);
-    return NextResponse.redirect(new URL("/login", req.url));
-  } */
+    const verified = jwt.verify(token, JWT_SECRET);
+    if (verified) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 export const config = {
-  matcher: "/api/:path*",
+  matcher: ["/protected/:path*", "/api/:path*"],
 };
