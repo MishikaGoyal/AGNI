8dd23e6e1d68cf628f048c24500d52c0b433b36e
MishikaGoyal <mishikaaagarwal@gmail.com>
2024-09-07 18:15:13 +0530

    Result Column Code Added

--- model/datasets/result.py
+++ model/datasets/result.py
@@ -1,16 +1,45 @@
-'''
-Result= ODD if:
-    Total Teachers * 40  > TotalStudents
-    CWSN < 1 or CWSN not in record
-    Separate Room for HM != 1
-    Grade Configuration not in [(1,5),(1,10),(1,12),(6,10),(11,12)]
-    School Type = 3 Co Educational and washroom < (1,1)
-    Boundary Wall != 1
-    Library Available != 1
-    Drinking Water Available != 1
-    Playground Available != 1
-    Electricity Availability != 1
-    Total Classrooms < Total Teachers
-else:
-    Result=Standard
-'''
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Data'] 
+collection = db['School'] 
+
+def check_conditions(record):
+    total_teachers = int(record.get("Total Teachers", 0))
+    total_students = int(record.get("Total Students", 0))
+    cwsn = int(record.get("CWSN", 0))
+    separate_room_for_hm = int(record.get("Separate Room for HM", 0))
+    grade_configuration = tuple(map(int, record.get("Grade Configuration", "0-0").split('-')))
+    school_type = int(record.get("School Type", 0))
+    total_washrooms = tuple(map(int, record.get("Total Washrooms", "0-0").split('-')))
+    boundary_wall = int(record.get("Boundary Wall", 0))
+    library_available = int(record.get("Library Available", 0))
+    drinking_water_available = int(record.get("Drinking Water Available", 0))
+    playground_available = int(record.get("Playground Available", 0))
+    electricity_availability = int(record.get("Electricity Availability", 0))
+    total_classrooms = int(record.get("Total Class Rooms", 0))
+
+    if (total_teachers * 40 > total_students or
+        cwsn < 1 or
+        separate_room_for_hm != 1 or
+        grade_configuration not in [(1, 5), (1, 10), (1, 12), (6, 10), (11, 12)] or
+        (school_type == 3 and (total_washrooms[0] < 1 or total_washrooms[1] < 1)) or
+        boundary_wall != 1 or
+        library_available != 1 or
+        drinking_water_available != 1 or
+        playground_available != 1 or
+        electricity_availability != 1 or
+        total_classrooms < total_teachers):
+        return "ODD"
+    else:
+        return "Standard"
+
+for record in collection.find():
+    result = check_conditions(record)
+    # Update or add the 'Result' field in the record
+    collection.update_one(
+        {"_id": record["_id"]},  # Use the unique identifier to find the record
+        {"$set": {"Result": result}}
+    )
+
+print("Records updated successfully.")
