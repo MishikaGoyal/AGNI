b17e60cad21fb7a32d64f4bd37fb548ee05737d0
MishikaGoyal <mishikaaagarwal@gmail.com>
2024-09-08 00:45:45 +0530

    Data Extraction Complete

--- model/datasets/dataExtraction.py
+++ model/datasets/dataExtraction.py
@@ -1,7 +1,8 @@
 import PyPDF2
 import re
-import json
 import os
+import json
+
 def extract_school_data(pdf_file):
     with open(pdf_file, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
@@ -14,10 +15,10 @@ def extract_school_data(pdf_file):
         "UDISE CODE": r'UDISE CODE\s*:\s*(\d{2}\s+\d{2}\s+\d{2}\s+\d{2}\s+\d{3})',
         "School Name": r'School Name\s*:\s*(.+)',
         "State": r'State\s+([A-Za-z]+)',
-        "School Category": r'School Category\s*(\d+)',  # Extracting only the number
-        "School Management": r'School Management\s*(\d+\s*-\s*.+)',
-        "School Type": r'School Type\s*(\d+\s*-\s*.+)',
-        "Grade Configuration": r'Lowest & Highest Class\s*(\d+)\s*-\s*(\d+)',  # Adjusting format below
+        "School Category": r'School Category\s*(\d+)',  
+        "School Management": r'School Management\s*(\d+)',
+        "School Type": r'School Type\s*(\d+)',  
+        "Grade Configuration": r'Lowest & Highest Class\s*(\d+)\s*-\s*(\d+)',  
         "Year of Establishment": r'Year of Establishment\s*(\d{4})',
         "Boundary Wall": r'Boundary wall\s*(\d+)',
         "Total Class Rooms": r'Total Class Rooms\s*(\d+)',
@@ -27,8 +28,8 @@ def extract_school_data(pdf_file):
         "Playground Available": r'Playground Available\s*(\d+)',
         "Electricity Availability": r'Electricity Availability\s*(\d+)',
         "Total Teachers": r'Total\s*(\d+)\s*',
+        "Total Washrooms": r'Total\(Excluding CWSN\)\s*(\d+)\s*(\d+)',
         "CWSN": r'Cwsn*\s(\d+)(?:\s|$)',
-        "Total Washrooms": r'Total\(Excluding CWSN\)\s*:?[\s\n]*(\d+)\s+(\d+)',
         "Total Students": r'G\.Tot.*\s(\d+)(?:\s|$)'
     }
 
@@ -38,21 +39,20 @@ def extract_school_data(pdf_file):
     for key, pattern in patterns.items():
         match = re.search(pattern, text)
         if match:
-            if key in ['Total Toilets (Excluding CWSN)', 'Lowest & Highest Class']:
-                # Formatting the output to "1-7"
-                extracted_data[key] = f"{match.group(1)}-{match.group(2)}"
+            if key == "Grade Configuration":
+                extracted_data["Grade Configuration"] = f"({match.group(1)},{match.group(2)})"
+            elif key == "Total Washrooms":
+                extracted_data["Total Washrooms"] = f"({match.group(1)},{match.group(2)})"
             else:
                 extracted_data[key] = match.group(1)
 
     return extracted_data
 
-# Define the folder path containing the PDF files
-folder_path = "./School Downloads"
+folder_path = "C:/Users/Hp/Desktop/Working/SIH 2024/Data/Schools"
 
 # Create a list to store the extracted data from each PDF
 all_school_data = []
 
-# Iterate over all PDF files in the folder
 for filename in os.listdir(folder_path):
     if filename.endswith(".pdf"):
         pdf_file_path = os.path.join(folder_path, filename)
@@ -60,18 +60,9 @@ for filename in os.listdir(folder_path):
         school_data = extract_school_data(pdf_file_path)
         all_school_data.append(school_data)
 
-# Define the output JSON file path
-output_json_path = "./schools_data.json"
+output_json_path = "model/datasets/schools_data.json"
 
-# Save the extracted data to a JSON file
 with open(output_json_path, "w") as json_file:
     json.dump(all_school_data, json_file, indent=4)
 
 print(f"Data extracted and saved to {output_json_path}")
-
-'''
-Errors to be resolved:
-School Type should only be number as in 3 not 3-co-educational
-Grade configuration bhi glt store hai it should be in the form of 1-12 smth but it's showing only one number
-I added number of Washrroms waala part but check for its format in json file.
-'''
