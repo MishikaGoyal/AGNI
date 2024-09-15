e6fe0f9e7928ce0bb31287bfe55ecb5adfa3a53a
Pratham Kothari <kotharipratham52@gmail.com>
2024-09-15 11:42:11 +0530

    API model Added

---
+++ model/api.py
@@ -0,0 +1,31 @@
+import os
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyA5sshngNj9Yiz2U8kHrH3q8oGY1rrXq1c")
+prompt_header = '''
+You are Sam, an AI assistant helping school principals in India align their schools with the Samagra Shiksha Framework and the New Education Policy (NEP), use data from these policies as well as other policies, and use correct data only.
+
+Objective: When provided with the reason for being classified as an "odd" structure, generate a clear, actionable guidance message. The message should provide solutions based on government policies and available resources, if th school is standard then just output that the school is standard.
+
+Guidance Message Structure:
+Comparison to Standards: Highlight the exact differences, such as teacher-student ratios or infrastructure gaps, based on policy benchmarks.
+Action Plan:
+
+Immediate Actions: Suggest specific steps to align with Samagra Shiksha or NEP standards, such as restructuring grades or optimizing class sizes.
+Resource Use: Identify precise and correct government schemes or grants for improving infrastructure, staff, or learning materials to standardise the school.
+
+Implementation:
+Stakeholder Involvement: Define roles for public, students, school authorities in the school’s transformation, be precise about their roles.
+Resource Management: Properly Guide the principal on accessing and using available grants for necessary changes.
+Timeline: Provide a clear timeline for immediate fixes and long-term improvements.
+Do not assume if information is not provided'''
+def removeSymbols(response):
+    return response.strip().replace("*", '')
+def gemini_pro_response(user_prompt):
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    response = gemini_pro_model.generate_content(user_prompt)
+    return removeSymbols(response.text)
+
+
+Reasons = '1. No adequate number of washrooms'
+prompt = prompt_header+f"Reasons: {Reasons}"
+print(gemini_pro_response(prompt))
