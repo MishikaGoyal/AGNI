3978ea66b48550ad99cc6a987d1c6f7d0238cceb
ashab-k <ashabkhan2000@gmail.com>
2024-09-15 10:20:33 +0530

    minor changes

--- website/src/app/(root)/principal/page.jsx
+++ website/src/app/(root)/principal/page.jsx
@@ -34,7 +34,8 @@ export default function Page() {
         <Navbar1 />
         <div className="flex flex-col md:flex-row items-center justify-evenly mt-12 space-y-4 md:space-y-0 md:space-x-8">
           <h1 className="tracking-in-expand-fwd text-xl font-extrabold text-center md:text-left">
-            Get All your School Information <br /> and upgrade your school to standard
+            Get All your School Information <br /> and upgrade your school to
+            standard
           </h1>
           <Image
             src="/img6.jpg"
@@ -54,7 +55,9 @@ export default function Page() {
               />
             </figure>
             <div className="card-body p-4">
-              <h2 className="card-title text-xl font-semibold">Know Your School</h2>
+              <h2 className="card-title text-xl font-semibold">
+                Know Your School
+              </h2>
               <Link href="/principal/structure">
                 <button className="btn btn-primary mt-4">See Now</button>
               </Link>
@@ -64,18 +67,25 @@ export default function Page() {
           {/* Text - 1 */}
           <div className="w-full md:w-[600px] p-4  ">
             <p className="text-md text-left leading-relaxed ">
-              This feature leverages a machine learning model to evaluate key school attributes like infrastructure, student-to-teacher ratio, curriculum quality, and safety measures. Schools are classified as "odd" if they significantly deviate from standard norms, or "standard" if they align with accepted educational criteria
+              This feature leverages a machine learning model to evaluate key
+              school attributes like infrastructure, student-to-teacher ratio,
+              curriculum quality, and safety measures. Schools are classified as
+              "odd" if they significantly deviate from standard norms, or
+              "standard" if they align with accepted educational criteria
             </p>
           </div>
         </div>
-
-
 
         <div className="flex flex-col md:flex-row items-center justify-evenly p-4 mb-4 shadow-lg rounded-lg bg-gray-200 w-[90%] mx-auto">
           {/* Text - 2 */}
           <div className="w-full md:w-[600px] p-4 flex items-center">
             <p className="text-md leading-relaxed">
-              An "Update School Information" feature allows users to revise key school data, including infrastructure, enrollment, teacher qualifications, and safety protocols. Keeping this data current ensures accurate analysis and reliable school classifications. Updates also prompt the machine learning model to re-evaluate the school’s classification, maintaining accuracy.
+              An "Update School Information" feature allows users to revise key
+              school data, including infrastructure, enrollment, teacher
+              qualifications, and safety protocols. Keeping this data current
+              ensures accurate analysis and reliable school classifications.
+              Updates also prompt the machine learning model to re-evaluate the
+              school’s classification, maintaining accuracy.
             </p>
           </div>
 
@@ -91,17 +101,17 @@ export default function Page() {
             <div className="card-body flex flex-col justify-between h-1/3 p-4">
               <h2 className="card-title text-xl font-semibold">Mark Updates</h2>
               <p className="mt-2 text-sm">
-                If any changes are made by you in your school during this period, please update us!
+                If any changes are made by you in your school during this
+                period, please update us!
               </p>
               <div className="card-actions mt-auto flex justify-end">
-                <Link href="principal/Progress">
+                <Link href="principal/progress">
                   <button className="btn btn-primary">Update Now</button>
                 </Link>
               </div>
             </div>
           </div>
         </div>
-
 
         <div className="flex flex-col md:flex-row items-start justify-evenly p-4 mb-4 bg-gray-200 shadow-lg rounded-lg border border-gray-200 w-[90%] mx-auto">
           {/* Card 3 */}
@@ -114,12 +124,20 @@ export default function Page() {
               />
             </figure>
             <div className="card-body flex flex-col h-full p-4">
-              <h2 className="card-title text-xl font-semibold">Request Resources</h2>
+              <h2 className="card-title text-xl font-semibold">
+                Request Resources
+              </h2>
               <p className="mt-4 text-sm">
-                Any resources required for upgrading your school from odd structure to standard. Send us a request and we will help you!
+                Any resources required for upgrading your school from odd
+                structure to standard. Send us a request and we will help you!
               </p>
               <div className="card-actions mt-auto flex justify-end">
-                <button onClick={() => sendMessage("/api/sendmessage2")} className="btn btn-primary">Make Request</button>
+                <button
+                  onClick={() => sendMessage("/api/sendmessage2")}
+                  className="btn btn-primary"
+                >
+                  Make Request
+                </button>
               </div>
             </div>
           </div>
@@ -127,18 +145,29 @@ export default function Page() {
           {/* Text Section - 3 */}
           <div className="w-full md:w-[600px] p-4 ">
             <p className="text-md text-left leading-relaxed">
-              The Resource Request feature enables schools to request essential resources like educational materials, infrastructure, and technology. It streamlines communication between schools and administrators, facilitating efficient management and allocation of resources. This centralized platform ensures access to necessary tools for quality education and improves transparency in resource distribution.
+              The Resource Request feature enables schools to request essential
+              resources like educational materials, infrastructure, and
+              technology. It streamlines communication between schools and
+              administrators, facilitating efficient management and allocation
+              of resources. This centralized platform ensures access to
+              necessary tools for quality education and improves transparency in
+              resource distribution.
             </p>
           </div>
         </div>
-
-
 
         <div className="flex flex-col md:flex-row items-center justify-evenly p-4 mb-4 shadow-lg rounded-lg bg-gray-200 w-[90%] mx-auto">
           {/* Text -4 */}
           <div className="w-full md:w-[600px] flex items-center">
             <p className="text-md leading-relaxed">
-              The "Guidelines for Upgradation" feature offers schools detailed steps to transition from "odd" to "standard" status. It includes customized recommendations, best practices, compliance checklists, and case studies to improve various aspects such as infrastructure, teaching methods, student engagement. By following these guidelines, schools can address gaps effectively, align with educational standards, and ensure continuous improvement and high-quality education.
+              The "Guidelines for Upgradation" feature offers schools detailed
+              steps to transition from "odd" to "standard" status. It includes
+              customized recommendations, best practices, compliance checklists,
+              and case studies to improve various aspects such as
+              infrastructure, teaching methods, student engagement. By following
+              these guidelines, schools can address gaps effectively, align with
+              educational standards, and ensure continuous improvement and
+              high-quality education.
             </p>
           </div>
 
@@ -154,20 +183,17 @@ export default function Page() {
             <div className="card-body flex flex-col justify-between h-1/3 p-4">
               <h2 className="card-title text-xl font-semibold">Guidelines</h2>
               <p className="mt-2 text-sm">
-                Any problem in upgrading? Go through these guidelines; they will help you for sure!
+                Any problem in upgrading? Go through these guidelines; they will
+                help you for sure!
               </p>
               <div className="card-actions mt-auto flex justify-end">
-                <Link href="/principal/Guidelines">
+                <Link href="/principal/guidelines">
                   <button className="btn btn-primary">Know How</button>
                 </Link>
               </div>
             </div>
           </div>
         </div>
-
-
-
-
       </div>
     </div>
   );
--- website/src/app/api/update/route.js
+++ website/src/app/api/update/route.js
@@ -4,11 +4,25 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 export async function POST(req) {
   const data = await req.json();
-  /* const update = await prisma.schoolUpdates.create({
+  const update = await prisma.schoolUpdates.create({
     data: {
       UDISE_CODE: data.UDISE_CODE,
+      State: data.State,
+      Grade_Configuration: data.Grade_Configuration,
+      Boundary_Wall: data.Boundary_Wall,
+      Total_Class_Rooms: data.Total_Class_Rooms,
+      Library_Available: data.Library_Available,
+      Separate_Room_for_HM: data.Separate_Room_for_HM,
+      Drinking_Water_Available: data.Drinking_Water_Available,
+      Playground_Available: data.Playground_Available,
+      Electricity_Availability: data.Electricity_Availability,
+      Total_Students: data.Total_Students,
+      Total_Teachers: data.Total_Teachers,
+      Total_Washrooms: data.Total_Washrooms,
+      Result: data.Result,
+      Verified: false,
     },
-  }); */
+  });
   console.log(data);
   return NextResponse.json(
     { message: "Update request sent successfully", flag: true },
